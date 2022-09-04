c0c0fbaed71c64073c550eb5191825935e83f9c5
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-09-04 17:11:29 +0200

    movie card component is added

--- movie-app/src/components/MovieSearchRow.js
+++ movie-app/src/components/MovieSearchRow.js
@@ -2,10 +2,8 @@ import React from "react";
 import { useFetch } from "../hooks/useFetch";
 import Loading from "./Loading";
 import Error from "./Error";
-import Poster from "./Poster";
+import { MovieCard } from "./MovieCard";
 import "../App.css";
-import { Link } from "react-router-dom";
-import favoriteIcon from "../assets/favorite2.png";
 import "../componentCss/Row.css";
 import { useSearchContext } from "../context/SearchContext";
 
@@ -28,23 +26,13 @@ const MovieSearchRow = ({ className }) => {
       {data.results.length > 0 && (
         <div className={className}>
           {data.results.map(({ id, title, poster_path, vote_average }) => (
-            <div className="movie-card" key={id}>
-              <Link to={`movie/${id}`}>
-                <Poster
-                  className="poster-link"
-                  posterPath={poster_path}
-                  alt={title}
-                />
-              </Link>
-              <div className="overlay">
-                <img
-                  className="favorite-icon"
-                  src={favoriteIcon}
-                  alt="favoriteIcon"
-                />
-                <p className="average-vote-count">{vote_average}</p>
-              </div>
-            </div>
+            <MovieCard
+              key={id}
+              id={id}
+              title={title}
+              posterPath={poster_path}
+              voteAverage={vote_average}
+            />
           ))}
         </div>
       )}
